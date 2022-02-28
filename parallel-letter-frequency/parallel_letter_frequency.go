61f411fcd1f45548d82dd56033d47aa57cbcfed4
Chris Dunphy <chris@megaparsec.ca>
2022-02-28 13:06:35 -0700

    Solved the parallel-letter-frequency Go problem

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,12 +1,26 @@
 package letter
 
+import (
+	"sync"
+)
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
+
+// LockedFreqMap adds a mutex to the map.
+type LockedFreqMap struct {
+	sync.RWMutex
+	internal FreqMap
+}
+
+func NewLockedFreqMap() *LockedFreqMap {
+	return &LockedFreqMap{internal: make(map[rune]int)}
+}
 
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
 func Frequency(s string) FreqMap {
-	m := FreqMap{}
+	m := make(FreqMap)
 	for _, r := range s {
 		m[r]++
 	}
@@ -16,5 +30,21 @@ func Frequency(s string) FreqMap {
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
 // by making use of concurrency.
 func ConcurrentFrequency(l []string) FreqMap {
-	panic("Implement the ConcurrentFrequency function")
+	var wg sync.WaitGroup
+	lm := NewLockedFreqMap()
+	for _, segment := range l {
+		wg.Add(1)
+		go doCalc(segment, &wg, lm)
+	}
+	wg.Wait()
+	return *&lm.internal
+}
+
+func doCalc(segment string, wg *sync.WaitGroup, m *LockedFreqMap) {
+	m.Lock()
+	defer m.Unlock()
+	defer wg.Done()
+	for _, r := range segment {
+		m.internal[r]++
+	}
 }
