4e7ec5c84a3a76c2f84c7c90ccfd1700d0af10fe
Chris Dunphy <chris@megaparsec.ca>
2022-03-22 10:48:18 -0600

    starting binary search tree

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -7,16 +7,6 @@ import (
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
-// LockedFreqMap adds a mutex to the map.
-type LockedFreqMap struct {
-	sync.RWMutex
-	internal FreqMap
-}
-
-func NewLockedFreqMap() *LockedFreqMap {
-	return &LockedFreqMap{internal: make(map[rune]int)}
-}
-
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
 func Frequency(s string) FreqMap {
@@ -30,21 +20,25 @@ func Frequency(s string) FreqMap {
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
 // by making use of concurrency.
 func ConcurrentFrequency(l []string) FreqMap {
+	freqChan := make(chan FreqMap, len(l))
 	var wg sync.WaitGroup
-	lm := NewLockedFreqMap()
-	for _, segment := range l {
-		wg.Add(1)
-		go doCalc(segment, &wg, lm)
-	}
-	wg.Wait()
-	return *&lm.internal
-}
+	wg.Add(len(l))
 
-func doCalc(segment string, wg *sync.WaitGroup, m *LockedFreqMap) {
-	m.Lock()
-	defer m.Unlock()
-	defer wg.Done()
-	for _, r := range segment {
-		m.internal[r]++
+	for _, s := range l {
+		go func(s string, wg *sync.WaitGroup) {
+			freqChan <- Frequency(s)
+			wg.Done()
+		}(s, &wg)
 	}
+
+	wg.Wait()
+	close(freqChan)
+
+	res := make(FreqMap)
+	for m := range freqChan {
+		for k, v := range m {
+			res[k] += v
+		}
+	}
+	return res
 }
