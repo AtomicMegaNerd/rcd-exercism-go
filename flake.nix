23cac289e7ad7f3ca92385a35fc2e241445945de
Chris Dunphy <chris@megaparsec.ca>
2023-02-26 20:56:41 -0700

    Make this a flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "This is a program representing rational numbers in Python";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell
+          pkgs.mkShell
+          {
+            # The packages we need for this project
+            buildInputs = with pkgs;
+              [
+                go
+                go-tools
+                gopls
+                exercism
+              ];
+          };
+      });
+}
